b3cfde1ceae5d384bca53e6eb9b1efc689eb7523
Saul Gomes <klowd.bk@gmail.com>
2023-04-03 00:58:06 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import * as cors from 'cors';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cors({
+      origin: 'http://localhost:3001',
+      credentials: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
